7931b414d6d2c34729a2873d1463274eaac0b054
jaeyong1@naver.com <jaeyong1nodejs@jyptest1.cafe24app.com>
2018-10-01 13:32:37 +0900

    add '전체Form 마우스드래그로 이동'

--- GolfManager/Form1.vb
+++ GolfManager/Form1.vb
@@ -60,4 +60,39 @@
 
     End Sub
 
+    '전체Form 마우스드래그로 이동
+    Public MoveForm As Boolean
+    Public MoveForm_MousePosition As Point
+
+    '전체Form 마우스드래그로 이동
+    Public Sub MoveForm_MouseDown(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseDown ' Add more handles here (Example: PictureBox1.MouseDown)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = True
+            Me.Cursor = Cursors.NoMove2D
+            MoveForm_MousePosition = e.Location
+        End If
+
+    End Sub
+    '전체Form 마우스드래그로 이동
+    Public Sub MoveForm_MouseMove(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseMove ' Add more handles here (Example: PictureBox1.MouseMove)
+
+        If MoveForm Then
+            Me.Location = Me.Location + (e.Location - MoveForm_MousePosition)
+        End If
+
+    End Sub
+    '전체Form 마우스드래그로 이동
+    Public Sub MoveForm_MouseUp(sender As Object, e As MouseEventArgs) Handles _
+    MyBase.MouseUp ' Add more handles here (Example: PictureBox1.MouseUp)
+
+        If e.Button = MouseButtons.Left Then
+            MoveForm = False
+            Me.Cursor = Cursors.Default
+        End If
+
+    End Sub
+
 End Class
